d72371ea3451af32c28ea43122ed37d66a96e10a
Nicron <nicron007@gmail.com>
2025-11-10 03:40:34 +0000

    feat(main-page): add filter functionality and some style improvements

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,11 +3,19 @@
 import PrimaryButton from "@/components/buttons/PrimaryButton";
 import TaskCard from "@/components/cards/TaskCard";
 import CreateTaskModal from "@/components/modals/CreateTaskModal";
-import { CirclePlus, PackageOpen, LoaderCircle, Search } from "lucide-react";
+import {
+  CirclePlus,
+  PackageOpen,
+  LoaderCircle,
+  SearchX,
+  ClipboardList,
+} from "lucide-react";
 import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import { formatDate } from "@/utils/dateFormatter";
 import useTasks from "@/hooks/useTasks";
+import SearchBar from "@/components/inputs/SearchBar";
+import CustomSelect from "@/components/inputs/CustomSelect";
 
 export default function Home() {
   const {
@@ -18,9 +26,27 @@ export default function Home() {
     updateTask,
     switchCompleteTask,
     searchTasks,
+    pendingTasks,
+    completedTasks,
   } = useTasks();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+  const [selected, setSelected] = useState("all");
+
+  const options = [
+    { label: "Todas", value: "all" },
+    { label: "Pendientes", value: "pending" },
+    { label: "Completadas", value: "complete" },
+  ];
+
+  const filteredTasks =
+    selected === "all"
+      ? tasks
+      : selected === "pending"
+      ? pendingTasks
+      : completedTasks;
+
   const handleModalOpen = () => {
     setIsModalOpen(!isModalOpen);
   };
@@ -28,6 +54,7 @@ export default function Home() {
     if (searchQuery.trim() === "") {
       return;
     }
+    setIsSearching(true);
     searchTasks(searchQuery);
   };
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -40,38 +67,46 @@ export default function Home() {
     const value = e.target.value;
     setSearchQuery(value);
     if (value.trim() === "") {
+      setIsSearching(false);
       searchTasks("");
     }
   };
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center">
-      <main className="w-[95%] flex flex-col items-center py-15 max-w-[1400px]">
-        <h1 className="text-4xl font-bold mb-3">Lista de Tareas</h1>
-        <div className="flex items-center justify-center gap-5 mb-5 w-full">
-          <PrimaryButton
-            action={handleModalOpen}
-            text="Crear Tarea"
-            icon={<CirclePlus size={20} />}
-          />
+    <div className="flex flex-col min-h-screen items-center">
+      <header className="w-full h-20 bg-light-blue flex items-center justify-center">
+        <div className="flex items-center max-w-[1400px] w-[95%]">
           <div
-            className="relative focus-within:ring-1 focus-within:ring-light-blue py-2 px-4 pr-10
-           rounded-lg border border-light-gray focus-within:border-light-blue transition-all duration-300"
+            className="h-14 w-14 flex items-center justify-center rounded-full border-4
+           border-dark-blue"
           >
-            <input
-              value={searchQuery}
-              onChange={handleInputChange}
-              onKeyDown={handleKeyPress}
-              type="text"
-              className="focus:outline-none w-full"
-              placeholder="Buscar tarea"
-            />
-            <div
-              onClick={handleSearch}
-              className="hover:text-medium-blue transition-all duration-300 flex items-center justify-center px-3 h-10.5 absolute right-0 top-1/2 -translate-y-1/2 cursor-pointer text-dark-gray "
-            >
-              <Search size={20} />
-            </div>
+            <ClipboardList size={38} />
           </div>
+        </div>
+      </header>
+      <main className="w-[95%] flex flex-col items-center py-15 max-w-[1400px]">
+        <h1 className="text-4xl font-bold text-center">Lista de Tareas</h1>
+        <p className="mb-5 text-dark-gray text-lg text-center">
+          Crea y administra tus tareas de forma fácil y eficiente
+        </p>
+        <div className="flex sm:flex-row flex-col items-center justify-center gap-5 mb-10 w-full">
+          <div className="w-full sm:w-50 flex justify-center sm:justify-end">
+            <PrimaryButton
+              action={handleModalOpen}
+              text="Crear Tarea"
+              icon={<CirclePlus size={20} />}
+            />
+          </div>
+          <SearchBar
+            searchQuery={searchQuery}
+            handleInputChange={handleInputChange}
+            handleKeyPress={handleKeyPress}
+            handleSearch={handleSearch}
+          />
+          <CustomSelect
+            options={options}
+            value={selected}
+            onChange={setSelected}
+          />
         </div>
 
         {loading && (
@@ -88,38 +123,65 @@ export default function Home() {
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
-            className="w-full text-center h-100 bg-light-blue/70 flex flex-col items-center justify-center 
+            className="w-full text-center p-4 h-50 flex flex-col items-center justify-center 
           rounded-lg"
           >
-            <PackageOpen size={110} />
-            <h2 className="text-2xl font-bold">
-              Aún no tienes tareas agregadas
-            </h2>
-            <p className="text-dark-gray">Crea una nueva tarea para comenzar</p>
+            {isSearching ? (
+              <>
+                <SearchX size={110} />
+                <h2 className="text-2xl font-bold">No se encontraron tareas</h2>
+                <p className="text-dark-gray">
+                  Intenta con otra búsqueda o verifica la ortografía
+                </p>
+              </>
+            ) : selected === "pending" ? (
+              <>
+                <PackageOpen size={110} />
+                <h2 className="text-2xl font-bold">No hay tareas pendientes</h2>
+              </>
+            ) : selected === "complete" ? (
+              <>
+                <PackageOpen size={110} />
+                <h2 className="text-2xl font-bold">
+                  No hay tareas completadas
+                </h2>
+              </>
+            ) : (
+              <>
+                <PackageOpen size={110} />
+                <h2 className="text-2xl font-bold">
+                  Aún no tienes tareas agregadas
+                </h2>
+                <p className="text-dark-gray">
+                  Crea una nueva tarea para empezar
+                </p>
+              </>
+            )}
           </motion.div>
         )}
         <motion.div className="w-full custom-grid">
           <AnimatePresence mode="popLayout">
-            {tasks.map((task) => (
-              <motion.div
-                initial={{ opacity: 0, y: -10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                layout
-                key={task.id}
-              >
-                <TaskCard
-                  date={formatDate(task.created_at)}
-                  title={task.title}
-                  description={task.description}
-                  id={task.id}
-                  completed={task.completed}
-                  deleteTask={deleteTask}
-                  updateTask={updateTask}
-                  switchCompleteTask={switchCompleteTask}
-                />
-              </motion.div>
-            ))}
+            {!loading &&
+              filteredTasks.map((task) => (
+                <motion.div
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  layout
+                  key={task.id}
+                >
+                  <TaskCard
+                    date={formatDate(task.created_at)}
+                    title={task.title}
+                    description={task.description}
+                    id={task.id}
+                    completed={task.completed}
+                    deleteTask={deleteTask}
+                    updateTask={updateTask}
+                    switchCompleteTask={switchCompleteTask}
+                  />
+                </motion.div>
+              ))}
           </AnimatePresence>
         </motion.div>
       </main>
